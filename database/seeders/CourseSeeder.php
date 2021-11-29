155467b08eb741022740df4b1d8f672516255ae9
amira <you@example.com>
2021-11-29 19:32:43 +0200

    ###

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -28,6 +28,15 @@ class CourseSeeder extends Seeder
         ]);
         Course::create([
             'cat_id'=>'2',
+            'name'=>'Photoshop',
+            'trainer_id'=>'3',
+            'small_des'=>'The world’s best imaging and graphic design software is at the core of just about every creative project, from photo editing and compositing to digital painting, animation, and graphic design.',
+            'des'=>'Photoshop on desktop and iPad, 100GB of cloud storage, Adobe Fresco, Adobe Portfolio, Adobe Fonts, and access to the latest features and updates as soon as they’re released ',
+            'price'=>'4000 ',
+            'photo'=>'ph.jpeg',
+        ]);
+        Course::create([
+            'cat_id'=>'3',
             'name'=>'English',
             'trainer_id'=>'2',
             'small_des'=>'This course emphasizes the fundamental language skills of reading, writing, speaking, listening, thinking, viewing',
@@ -35,15 +44,6 @@ class CourseSeeder extends Seeder
                 viewing and presenting. An emphasis on vocabulary and composition skills will be an on-going part of the program.',
             'price'=>'4000',
             'photo'=>'e.png',
-        ]);
-        Course::create([
-            'cat_id'=>'3',
-            'name'=>'Photoshop',
-            'trainer_id'=>'3',
-            'small_des'=>'The world’s best imaging and graphic design software is at the core of just about every creative project, from photo editing and compositing to digital painting, animation, and graphic design.',
-            'des'=>'Photoshop on desktop and iPad, 100GB of cloud storage, Adobe Fresco, Adobe Portfolio, Adobe Fonts, and access to the latest features and updates as soon as they’re released ',
-            'price'=>'4000 ',
-            'photo'=>'ph.jpeg',
         ]);
     }
 }
